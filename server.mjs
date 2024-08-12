92443c474a7d3b54906c16c60fc53e45642affa1
hinagulati <hina.gulatii@gmail.com>
2024-08-12 11:46:43 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -136,7 +136,7 @@ app.get('/api/update-product-fabric', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
