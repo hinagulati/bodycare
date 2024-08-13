c8ad87dabdf4e4c795af057771b9569eb108b204
hinagulati <hina.gulatii@gmail.com>
2024-08-13 12:23:39 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -139,7 +139,7 @@ const extractFabricValue = (htmlContent) => {
 
 
 // Function to process a batch of products
-const processProducts = async (limit = 500) => {
+const processProducts = async (limit = 250) => {
   let processedCount = 0;
   let hasMoreProducts = true;
   let lastProductId = null;
@@ -217,7 +217,7 @@ const processProducts = async (limit = 500) => {
 
 // Define an API endpoint to trigger the update for a specific number of products
 app.get('/api/update-product-fabric', async (req, res) => {
-  const limit = parseInt(req.query.limit, 10) || 500; // Default to 50 products if not specified
+  const limit = parseInt(req.query.limit, 10) || 250; // Default to 50 products if not specified
 
   try {
     await processProducts(limit);
